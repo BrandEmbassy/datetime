949d56374861680efc20b3f4bfb72accbcc80a5b
Dominik Voda <d.voda94@gmail.com>
2021-05-18 15:18:01 +0200

    Fix RFC parsing + php versioning in tests (#19)

--- src/Format/Rfc3339ExtendedFormat.php
+++ src/Format/Rfc3339ExtendedFormat.php
@@ -3,7 +3,6 @@
 namespace BrandEmbassy\DateTime\Format;
 
 use DateTime;
-use const PHP_VERSION_ID;
 
 final class Rfc3339ExtendedFormat
 {
@@ -15,8 +14,6 @@ final class Rfc3339ExtendedFormat
 
     public static function getInputFormat(): string
     {
-        return PHP_VERSION_ID >= 70300
-            ? DateTime::RFC3339_EXTENDED
-            : 'Y-m-d\TH:i:s.uP';
+        return 'Y-m-d\TH:i:s.uP';
     }
 }
