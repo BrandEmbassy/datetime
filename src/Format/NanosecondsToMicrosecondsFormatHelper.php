f5cc0567ded7174e50f699a7d6cabaa3a6c0a359
vharnach <vharnach@gmail.com>
2021-11-10 11:34:28 +0100

    Total simplification

--- src/Format/NanosecondsToMicrosecondsFormatHelper.php
+++ src/Format/NanosecondsToMicrosecondsFormatHelper.php
@@ -8,23 +8,21 @@ use function preg_match;
 
 final class NanosecondsToMicrosecondsFormatHelper
 {
-    private const INPUT_WITH_NANOSECONDS_PATTERN = '/^\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d{7,9}(Z|(\+|\-)[0,1]\d:00)$/';
     private const MICROSECONDS_AND_TIMEZONE_PATTERN = '/^(^\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d{6})\d*(Z|(?:\+|\-)[0,1]\d:00)$/';
 
 
     public static function normalizeInputIfNeeded(string $dateTimeAsStringWithNanoseconds): string
     {
-        if (preg_match(self::INPUT_WITH_NANOSECONDS_PATTERN, $dateTimeAsStringWithNanoseconds) === false) {
-            return $dateTimeAsStringWithNanoseconds;
+        if (preg_match(
+                self::MICROSECONDS_AND_TIMEZONE_PATTERN,
+                $dateTimeAsStringWithNanoseconds,
+                $dateTimeParts
+            ) === 1) {
+            array_shift($dateTimeParts);
+
+            return implode('', $dateTimeParts);
         }
 
-        preg_match(self::MICROSECONDS_AND_TIMEZONE_PATTERN, $dateTimeAsStringWithNanoseconds, $truncatedParts);
-        if ($truncatedParts === []) {
-            return $dateTimeAsStringWithNanoseconds;
-        }
-
-        array_shift($truncatedParts);
-
-        return implode('', $truncatedParts);
+        return $dateTimeAsStringWithNanoseconds;
     }
 }
