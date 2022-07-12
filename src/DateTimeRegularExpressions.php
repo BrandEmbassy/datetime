38d7e1c330c9be270b209303ade4ff9b6403e3e2
milan-zelenka <26469679+milan-zelenka@users.noreply.github.com>
2022-07-12 13:23:16 +0200

    Added DateTimeRegularExpressions with RFC3339 (#23)

---
+++ src/DateTimeRegularExpressions.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\DateTime;
+
+use Nette\StaticClass;
+
+/**
+ * @final
+ */
+class DateTimeRegularExpressions
+{
+    use StaticClass;
+
+    // RFC3339 without milliseconds - non-extended
+    // Compliant with \DateTimeInterface::RFC3339
+    public const RFC3339 = '#^([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(Z|[+-](2[0-3]|[01][0-9]):[0-5][0-9])$#';
+}
---
+++ src/DateTimeRegularExpressionsTest.php
@@ -0,0 +1,141 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\DateTime;
+
+use Nette\Utils\Strings;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @final
+ */
+class DateTimeRegularExpressionsTest extends TestCase
+{
+    /**
+     * @dataProvider dateTimeStringsDataProvider
+     */
+    public function testDateTimeStringMatchesPattern(
+        bool $expectedMatch,
+        string $pattern,
+        string $dateTime
+    ): void {
+        $doesMatch = Strings::match($dateTime, $pattern) !== null;
+
+        Assert::assertSame($expectedMatch, $doesMatch);
+    }
+
+
+    /**
+     * @return array<string, array<string, mixed>>
+     */
+    public function dateTimeStringsDataProvider(): array
+    {
+        return [
+            'RFC3339 | Valid DateTime with specified positive timezone' => [
+                'expectedMatch' => true,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-31T23:59:59+01:00',
+            ],
+            'RFC3339 | Valid DateTime with specified negative timezone' => [
+                'expectedMatch' => true,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-31T23:59:59-01:00',
+            ],
+            'RFC3339 | Valid DateTime with "Zulu" (UTC offset of 00:00) timezone' => [
+                'expectedMatch' => true,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-31T23:59:59Z',
+            ],
+            'RFC3339 | Invalid milliseconds' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-31T23:59:59.123Z',
+            ],
+            'RFC3339 | Invalid characters before DateTime' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => 'abc2000-12-31T23:59:59Z',
+            ],
+            'RFC3339 | Invalid characters after DateTime' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-31T23:59:59Zabc',
+            ],
+            'RFC3339 | Invalid year' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '19999-12-31T23:59:59Z',
+            ],
+            'RFC3339 | Invalid month - 0' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-0-31T23:59:59Z',
+            ],
+            'RFC3339 | Invalid month - 13' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-13-31T23:59:59Z',
+            ],
+            'RFC3339 | Invalid day - 0' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-0T23:59:59Z',
+            ],
+            'RFC3339 | Invalid day - 32' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-32T23:59:59Z',
+            ],
+            'RFC3339 | Invalid hour - 0' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-31T0:59:59Z',
+            ],
+            'RFC3339 | Invalid hour - 24' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-31T24:59:59Z',
+            ],
+            'RFC3339 | Invalid minute - 0' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-31T23:0:59Z',
+            ],
+            'RFC3339 | Invalid minute - 60' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-31T23:60:59Z',
+            ],
+            'RFC3339 | Invalid second - 0' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-31T23:59:0Z',
+            ],
+            'RFC3339 | Invalid second - 60' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-31T23:59:60Z',
+            ],
+            'RFC3339 | Invalid timezone hour - 0' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-31T23:59:59+0:00',
+            ],
+            'RFC3339 | Invalid timezone hour - 24' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-31T23:59:59+24:00',
+            ],
+            'RFC3339 | Invalid timezone minute - 0' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-31T23:59:59+01:0',
+            ],
+            'RFC3339 | Invalid timezone minute - 60' => [
+                'expectedMatch' => false,
+                'pattern' => DateTimeRegularExpressions::RFC3339,
+                'dateTime' => '2000-12-31T23:59:59+01:60',
+            ],
+        ];
+    }
+}
